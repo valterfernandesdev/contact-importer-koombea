0596e3320946533a65d17e20ea6d011430dae718
valterfernandesdev <valterfernandes.dev@gmail.com>
2022-11-12 18:04:17 -0300

    add missing test for contact belongs to user

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -5,6 +5,10 @@ require 'rails_helper'
 RSpec.describe Contact do
   subject(:contact) { build(:contact) }
 
+  describe "associations" do
+    it { is_expected.to belong_to(:user) }
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :date_of_birth }
